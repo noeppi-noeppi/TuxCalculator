deb998fc6877f8868af8742eb31eb86ae0b85d92
noeppi_noeppi <noeppinoeppi@gmail.com>
2023-12-10 18:49:10 +0100

    Clear the memoization cache on precision change, document pitfalls of #memoize in the manual and memoize the harmonic series function the plain format.

--- TuxCore/src/main/scala/tuxcalculator/core/function/MemoizedFunction.scala
+++ TuxCore/src/main/scala/tuxcalculator/core/function/MemoizedFunction.scala
@@ -9,16 +9,28 @@ import scala.ref.SoftReference
 class MemoizedFunction(val function: MathFunction) extends MathFunction {
   
   @transient
+  private[this] var _precision: Int = 0
+  
+  @transient
   private[this] var _cache: SoftReference[mutable.Map[Vector[MathValue], MathValue]] = SoftReference(mutable.Map())
+  
+  private def makeNewCache(): mutable.Map[Vector[MathValue], MathValue] = {
+    val map: mutable.Map[Vector[MathValue], MathValue] = mutable.Map()
+    _cache = SoftReference(map)
+    map
+  }
   
   private def cache: mutable.Map[Vector[MathValue], MathValue] = _cache.get match {
     case Some(map) => map
-    case None =>
-      val map: mutable.Map[Vector[MathValue], MathValue] = mutable.Map()
-      _cache = SoftReference(map)
-      map
+    case None => makeNewCache()
   }
   
-  override def applyTo(calc: Calculator, args: Vector[MathValue]): MathValue = cache.getOrElseUpdate(args, function.applyTo(calc, args))
+  override def applyTo(calc: Calculator, args: Vector[MathValue]): MathValue = {
+    if (_precision != calc.precision) {
+      makeNewCache()
+      _precision = calc.precision
+    }
+    cache.getOrElseUpdate(args, function.applyTo(calc, args))
+  }
   override def string(calc: Calculator): String = function.string(calc)
 }
