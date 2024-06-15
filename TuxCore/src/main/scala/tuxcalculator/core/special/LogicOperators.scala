ec78b69f59e19de5475565b802f9c173608c40c9
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-06-15 23:06:53 +0200

    Fix that 0 | true evaluated to 0.

--- TuxCore/src/main/scala/tuxcalculator/core/special/LogicOperators.scala
+++ TuxCore/src/main/scala/tuxcalculator/core/special/LogicOperators.scala
@@ -9,15 +9,15 @@ object LogicOperators {
   abstract class NumericBooleanOp(name: String, boolOp: (Boolean, Boolean) => Boolean, intOp: (BigInt, BigInt) => BigInt) extends CalculatorSpecial.SimpleFunction(name, 2) {
     override protected def result(calc: Calculator, args: Vector[MathValue]): MathValue = (args(0), args(1)) match {
       case (MathBoolean(b1), MathBoolean(b2)) => MathBoolean(boolOp(b1, b2))
-      case (MathBoolean(b1), MathRealNumeric(n2)) => MathNumber(BigDecimal(intOp(boolToInt(b1, n2.toBigInt), n2.toBigInt)))
-      case (MathRealNumeric(n1), MathBoolean(b2)) => MathNumber(BigDecimal(intOp(n1.toBigInt, boolToInt(b2, n1.toBigInt))))
+      case (MathBoolean(b1), MathRealNumeric(n2)) if n2.isWhole => MathNumber(BigDecimal(intOp(boolToInt(b1, n2.toBigInt), n2.toBigInt)))
+      case (MathRealNumeric(n1), MathBoolean(b2)) if n1.isWhole => MathNumber(BigDecimal(intOp(n1.toBigInt, boolToInt(b2, n1.toBigInt))))
       case (MathRealNumeric(n1), MathRealNumeric(n2)) if n1.isWhole && n2.isWhole => MathNumber(BigDecimal(intOp(n1.toBigInt, n2.toBigInt)))
       case (v1, v2)=> MathError("Can't " + name + " " + calc.format(v1) + " and " + calc.format(v2))
     }
     
     private def boolToInt(value: Boolean, other: BigInt): BigInt = {
       if (!value) 0
-      else BigInt(2).pow(other.abs.bitLength) - 1
+      else BigInt(2).pow(other.abs.bitLength `max` 1) - 1
     }
   }
   
