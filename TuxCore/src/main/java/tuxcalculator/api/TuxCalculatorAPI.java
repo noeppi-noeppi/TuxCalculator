30c2144879df160290499eadd65ad550e4f1551d
noeppi_noeppi <noeppinoeppi@gmail.com>
2024-11-05 17:52:31 +0100

    TuxCalculator 1.1.7

--- TuxCore/src/main/java/tuxcalculator/api/TuxCalculatorAPI.java
+++ TuxCore/src/main/java/tuxcalculator/api/TuxCalculatorAPI.java
@@ -8,7 +8,7 @@ import java.nio.file.Path;
  */
 public interface TuxCalculatorAPI {
 
-    String VERSION = "1.1.6";
+    String VERSION = "1.1.7";
 
     /**
      * Create a new ini-mode calculator.
